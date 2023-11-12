5f3e06c431dbaeeb71cf17697a0ffb27b00a7f30
ChiJhin <jerek.d.lem@gmail.com>
2023-11-12 19:07:49 +0200

    task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const listGallery = document.querySelector(".gallery");
+
+function renderGallery(items) {
+  return items
+    .map(
+      ({ description, original, preview }) =>
+        `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+    .join("");
+}
+
+listGallery.innerHTML = renderGallery(galleryItems);
+listGallery.addEventListener("click", onOpenModal);
+
+function onOpenModal(evt) {
+  if (evt.target === evt.currentTarget) {
+    return;
+  }
+
+  evt.preventDefault();
+
+  const instance = basicLightbox.create(
+    `<img src="${evt.target.dataset.source}" width="800" height="600">`
+  );
+  listGallery.addEventListener(
+    "keydown",
+    (evt) => {
+      if (evt.code === "Escape") {
+        instance.close();
+      }
+    },
+    { once: true }
+  );
+  instance.show();
+}
